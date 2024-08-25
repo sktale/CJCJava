a08e0856f616f1a9b3873a9cfb2be4f021a83a23
sktale <74125337+sktale@users.noreply.github.com>
2024-08-25 11:26:18 +0530

    Add files via upload
    data types

---
+++ Lect-3/lect3.java
@@ -0,0 +1,22 @@
+public class lect3{
+	
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static boolean bb;
+	public static void main(String [] ss){
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bb);
+		
+	}
+}
