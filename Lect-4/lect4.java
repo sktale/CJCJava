04d4fc4b9356889f950903136447e2fc67bcdf74
sktale <74125337+sktale@users.noreply.github.com>
2024-08-25 19:27:54 +0530

    Add files via upload
    Lect4

---
+++ Lect-4/lect4.java
@@ -0,0 +1,9 @@
+
+package com.sk.test;
+
+public class lect4{
+ public void m1()
+ {
+	 System.out.println("m2--B");
+ }
+}
---
+++ Lect-4/lect4A.java
@@ -0,0 +1,11 @@
+
+package com.sk.test2;
+import com.sk.test.*;
+public class lect4A{
+ public static void main(String[]ff)
+ {
+ lect4 a = new lect4();
+  System.out.println("Hello");
+  a.m1();
+ }
+}
